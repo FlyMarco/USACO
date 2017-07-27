108c4d058fd68981a4bb6fce94eb6bf8c4c5f8ce
FlyMarco <marco_lbh@163.com>
2017-07-27 17:07:12 +0800

    change code structure (#6)
    * pull (#3)
    
    * change code structure

--- FlyMarco/1/1.2/milk2.java
+++ FlyMarco/1/1.2/milk2.java
@@ -15,44 +15,42 @@ class milk2 {
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("milk2.out")));
 
 		int N = Integer.parseInt(f.readLine());
-		int[][] in = new int[N][2];
+		int[] temp = new int[4];
+		int[] result = new int[2];
 		for (int i = 0; i < N; i++) {
-			String temp = f.readLine();
-			in[i][0] = Integer.parseInt(temp.split(" ")[0]);
-			in[i][1] = Integer.parseInt(temp.split(" ")[1]);
+			String item = f.readLine();
+			int start = Integer.parseInt(item.split(" ")[0]);
+			int end = Integer.parseInt(item.split(" ")[1]);
+			if (i == 0) {
+				result[0] = end - start;
+				temp[0] = start;
+				temp[1] = end;
+			}
+			getResult(result, temp, start, end);
 		}
-		int[] result = getResult(N, in);
 		out.println(result[0] + " " + result[1]);
 		
 		out.close();
 	}
 
-	static int[] getResult(int N, int[][] in) {
-		
-		int startExist = in[0][0], endExist = in[0][1], startNo = -1, endNo = -1;
-		int[] res = new int[]{endExist - startExist,0};
-		for (int i = 1; i < N; i++) {
-			if ((in[i][0] > endExist || in[i][1] < startExist) ) {
-				if (in[i][1] - in[i][0] > res[0]) {
-					res[0] = in[i][1] - in[i][0];
-					startExist = in[i][0];
-					endExist = in[i][1];
-				}
-			} else if (in[i][0] <= endExist || in[i][1] >= startExist) {
-				startExist = Math.min(startExist, in[i][0]);
-				endExist = Math.max(endExist, in[i][1]);
-				res[0] = endExist - startExist;
+	static void getResult(int[] res, int[] temp, int start, int end) {
+		if ((start > temp[1] || end < temp[0]) && end - start >= res[0]) {
+			if (start > temp[1]) {
+				temp[2] = temp[1];
+				temp[3] = start;
+				res[1] = Math.max(res[1], temp[3] - temp[2]);
+			} else if (end < temp[0]) {
+				temp[2] = end;
+				temp[3] = temp[0];
+				res[1] = Math.max(res[1], temp[3] - temp[2]);
 			}
-			if (in[i][0] > endExist) {
-				startNo = endExist;
-				endNo = in[i][0];
-				res[1] = Math.max(res[1], endNo - startNo);
-			} else if (in[i][1] < startExist) {
-				startNo = in[i][1];
-				endNo = startExist;
-				res[1] = Math.max(res[1], endNo - startNo);
-			}
+			res[0] = end - start;
+			temp[0] = start;
+			temp[1] = end;
+		} else if (start <= temp[1] || end >= temp[0]) {
+			temp[0] = Math.min(temp[0], start);
+			temp[1] = Math.max(temp[1], end);
+			res[0] = temp[1] - temp[0];
 		}
-		return res;
 	}
 }
