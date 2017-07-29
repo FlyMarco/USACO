779ac406f0291a2ca0e1331b5a5557a31dcb1403
liubaohang <liu.baohang@innos.com>
2017-07-29 11:44:19 +0800

    update Java code style

--- FlyMarco/1/1.1/ride.java
+++ FlyMarco/1/1.1/ride.java
@@ -7,25 +7,25 @@ import java.io.*;
 import java.util.*;
 
 class ride {
-	public static void main (String [] args) throws IOException {
-		BufferedReader f = new BufferedReader(new FileReader("ride.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
-		String in1 = f.readLine();
-		String in2 = f.readLine();
-
-		if ((charToInt(in1) % 47) == (charToInt(in2) % 47)) {
-			out.println("GO");
-		} else {
-			out.println("STAY");
-		}
-		out.close();
-	}
-
-	static int charToInt(String in) {
-		int out = 1;
-		for (char c : in.toCharArray()) {
-			out = out * (c - 'A' + 1);
-		}
-		return out;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FlieReader("ride.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+        String in1 = f.readLine();
+        String in2 = f.readLine();
+        
+        if((charToInt(in1) % 47) == (charToInt(in2) % 47)) {
+            out.println("GO");
+        } else {
+            out.println("STAY");
+        }
+        out.close();
+    }
+    
+    static int chatToInt(String in) {
+        int out = 1;
+        for (char c : in.toCharArray()) {
+            out = out * (c - 'A' + 1);
+        }
+        return out;
+    }
 }
--- FlyMarco/1/1.2/milk2.java
+++ FlyMarco/1/1.2/milk2.java
@@ -7,40 +7,60 @@ import java.io.*;
 import java.util.*;
 
 class milk2 {
-	public static void main(String[] args) throws IOException {
-		test();
-	}
-	static void test() throws IOException {
-		BufferedReader f = new BufferedReader(new FileReader("milk2.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("milk2.out")));
-
-		int N = Integer.parseInt(f.readLine());
-		int MAX = 1000000;
-		int START = MAX + 1, END = -1;
-		int[] step = new int[MAX + 1];
-		for (int i = 0; i < N; i++) {
-			String item = f.readLine();
-			int start = Integer.parseInt(item.split(" ")[0]);
-			int end = Integer.parseInt(item.split(" ")[1]);
-			START = START > start ? start : START;
-			END = END > end ? END : end;
-			for (int j = start; j < end; j++) {
-				step[j] = 1;
-			}
-		}
-		int MAXEXIST = 0, MAXNO = 0, exist = 0, no = 0;
-		for (int i = START; i < END; i++) {
-			if (step[i] == 1) {
-				exist++;
-				no = 0;
-				MAXEXIST = MAXEXIST > exist ? MAXEXIST : exist;
-			} else {
-				exist = 0;
-				no++;
-				MAXNO = MAXNO > no ? MAXNO : no;
-      }
-		out.println(MAXEXIST + " " + MAXNO);
-
-		out.close();
-	}
+    
+    public static void main(String[] args) throws IOException {
+        test();
+    }
+    
+    static void test() throws IOException {
+        
+        BufferedReader f = new BufferedReader(new FileReader("transform.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("transform.out")));
+        
+        int N = Integer.parseInt(f.readLine());
+        int MAX = 1000000;
+        int START = MAX + 1, END = -1;
+        int[] step = new int[MAX + 1];
+        
+        for (int i = 0; i < N; i++) {
+            String item = f.readLine();
+            int start = Integer.parseInt(item.split(" ")[0]);
+            int end = Integer.parseInt(item.split(" ")[1]);
+            START = START > start ? start : START;
+            END = END > end ? END : end;
+            for (int j = start; j < end; j++) {
+                step[j] = 1;
+            }
+        }
+        
+        int MAXEXIST = 0, MAXNO = 0, exist = 0, no = 0;
+        
+        for (int i = START; i < END; i++) {          
+            if (step[i] == 1) {
+                exist++;
+                no = 0;
+                MAXEXIST = MAXEXIST > exist ? MAXEXIST : exist;
+            } else {
+                exist = 0;
+                no++;
+                MAXNO = MAXNO > no ? MAXNO : no;
+            }
+        }
+        out.println(MAXEXIST + " " + MAXNO);
+        
+        out.close();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
